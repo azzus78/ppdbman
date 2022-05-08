879e8fb96003b4d7a55f693ed5c129656fe8b15f
maroqijalil <jalilpro31@gmail.com>
2022-05-09 01:04:56 +0700

    test: try status form

--- user/mod_formulir/block.php
+++ user/mod_formulir/block.php
@@ -1,3 +1,6 @@
+<?php
+$jenis_data = fetch($koneksi, 'jenis', ['id_jenis' => $jenis]);
+?>
 <center>
 <div class="row">
     <div class="col-12">
@@ -9,7 +12,7 @@
                             <h3 style="color: #06628c;">Oops! Halaman Tidak Ditemukan</h3></br>
                             <img src="https://ethol.pens.ac.id/404.png" class="img-fluid" alt="Responsive image" draggable="false">
                         </div></br>
-                        <h3 style="color: #06628c;">Mohon Maaf Pengisian Formulir Jalur ....... Telah Selesai</h3></br>
+                        <h3 style="color: #06628c;">Mohon Maaf Pengisian Formulir Jalur <?= $jenis_data['nama_jenis'] ?> Telah Selesai</h3></br>
                         <a href="."><button type="button" class="btn btn-icon icon-left btn-dark">Kembali ke Beranda <i class="fas fa-home"></i></button></a>
                     </div> 
                 </div>
