4042211666c23c760bc4a98ced3cf9b9ed488085
root <root@vmi1042362.contaboserver.net>
2022-10-19 16:51:49 +0800

    Rubah Logo 404 || ke local server

--- user/mod_formulir/block.php
+++ user/mod_formulir/block.php
@@ -10,7 +10,7 @@ $jenis_data = fetch($koneksi, 'jenis', ['id_jenis' => $jenis]);
                     <div class="activity-detail">
                         <div>
                             <h3 style="color: #06628c;">Oops! Halaman Tidak Ditemukan</h3></br>
-                            <img src="https://ethol.pens.ac.id/404.png" class="img-fluid" alt="Responsive image" draggable="false">
+                            <img src="../assets/img/404.png" class="img-fluid" alt="Responsive image" draggable="false">
                         </div></br>
                         <h3 style="color: #06628c;">Mohon Maaf Pengisian Formulir Jalur <?= $jenis_data['nama_jenis'] ?> Telah Selesai</h3></br>
                         <a href="."><button type="button" class="btn btn-icon icon-left btn-dark">Kembali ke Beranda <i class="fas fa-home"></i></button></a>
